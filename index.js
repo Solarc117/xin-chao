d0e0d05003853fb231aee3af2e6e187b6e7a3703
solarc117 <jonathan.carlos.bernal@gmail.com>
2022-12-13 14:57:14 -0700

    feat: :sparkles: collapsed navbar for screens less than 900px wide

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-'use strict'
-const classNames = ['coffee', 'hot_chocolate', 'spring_rolls']
-let count = 0
+const query = document.querySelector.bind(document)
 
-setInterval(() => {
-  document.body.classList.add(classNames[count + 1 > 2 ? 0 : count + 1])
-  document.body.classList.remove(classNames[count])
-  count++
-  if (count > 2) count = 0
-}, 2000)
+function setDynamicBackground(document) {
+  const classNames = ['coffee', 'hot_chocolate', 'spring_rolls']
+  let count = 0
+
+  setInterval(() => {
+    document.body.classList.add(classNames[count + 1 > 2 ? 0 : count + 1])
+    document.body.classList.remove(classNames[count])
+    count++
+    if (count > 2) count = 0
+  }, 2000)
+}
+function showNav() {
+  console.log('showNavRunning')
+
+  query('.nav_links')?.classList.add('show')
+
+  for (const tag of [query('header'), query('main')])
+    tag.classList.add('dark_gradient')
+}
+function hideNav() {
+  console.log('hideNavRunning')
+
+  query('.nav_links')?.classList.remove('show')
+
+  for (const tag of [query('header'), query('main')])
+    tag.classList.remove('dark_gradient')
+}
+function toggleNav(event) {
+  const { target } = event
+
+  if (target.classList.contains('bars') || target.id === 'bars_path') showNav()
+  else hideNav()
+}
+
+setDynamicBackground(document)
+document.body.addEventListener('pointerdown', toggleNav)
