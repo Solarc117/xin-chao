c06470d4eb093d9d8906edc6ffce059225172671
solarc117 <jonathan.carlos.bernal@gmail.com>
2023-03-19 16:47:01 -0600

    chore: :heavy_plus_sign: install dotenv
    Installed dotenv as a dependency.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,23 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import * as dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 5500
 
 export default defineConfig({
+  server: {
+    port,
+  },
+  preview: {
+    port,
+  },
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         about: resolve(__dirname, 'about/about.html'),
-        contact: resolve(__dirname, 'contact/contact.html')
-      }
-    }
-  }
+        contact: resolve(__dirname, 'contact/contact.html'),
+      },
+    },
+  },
 })
